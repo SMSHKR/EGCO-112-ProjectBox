f7caec98b9bc131ab96ca4866c4809bfe6d404f1
SMSHKR <t.benzpun@gmail.com>
2018-05-05 23:13:33 +0700

    Prototype Menu

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <windows.h>
 
 #include "menu.hpp"
 #include "box.hpp"
 
-int main() {
-    //menu()
+using namespace std;
+
+int main()
+{
+    //**JUST IDEA**
+    bool check;
+    try
+    {
+        check = false;
+        do
+        {
+            system("cls");
+            int choice = menu();
+            switch (choice)
+            {
+            case 1: //maybe start game
+                break;
+            case 2: //maybe instruction/howto
+                break;
+            case 3: //meaybe exit?
+                break;
+            default: //throw exceoption
+                throw "cin >> ERROR";
+            }
+        } while (check);
+    }
+    catch (...)
+    {
+        cin.clear();
+        cin.ignore(50,'\n');
+        check = true;
+    }
     return 0;
 }
--- Source/menu.hpp
+++ Source/menu.hpp
@@ -1,8 +1,11 @@
 #ifndef MENU_HPP_INCLUDED 
 #define MENU_HPP_INCLUDED
 
-void menu() {
-    
+int menu() {
+    //add some fancy menu here
+    int choice;
+    std::cin >> choice; //may change workaround to sth like getch()
+    return choice;
 }
 
 #endif // MENU_HPP_INCLUDED
