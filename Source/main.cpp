b0052337ad1fa8771c1bdc0126a6b5740f8d3897
SMSHKR <t.benzpun@gmail.com>
2018-05-05 23:32:22 +0700

    Add gotoxy()

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,6 +5,15 @@
 #include "box.hpp"
 
 using namespace std;
+//someone please explain to me how this code work please...
+void gotoxy(int x, int y)
+{
+  static HANDLE h = NULL;  
+  if(!h)
+    h = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD c = { x, y };  
+  SetConsoleCursorPosition(h,c);
+}
 
 int main()
 {
