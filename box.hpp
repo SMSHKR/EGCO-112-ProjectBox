86c9c284194fa29a8b18e3916b7032ae338402c6
SMSHKR <t.benzpun@gmail.com>
2018-05-05 22:35:37 +0700

    Prototype box.h

--- box.hpp
+++ box.hpp
@@ -1,6 +1,11 @@
-#ifndef TEMPLATE_HPP_INCLUDED 
-#define TEMPLATE_HPP_INCLUDED
+#ifndef BOX_HPP_INCLUDED 
+#define BOX_HPP_INCLUDED
 
+class box {
+private:
+    int key;
+public:
+    void swap();
+};
 
-
-#endif // TEMPLATE_HPP_INCLUDED
+#endif // BOX_HPP_INCLUDED
