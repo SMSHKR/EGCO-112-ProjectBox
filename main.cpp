79d73142d6983a5a3100eccc35bf3fda039b328f
taninjackie <taninjackie@hotmail.com>
2018-05-06 23:41:25 +0700

    เช็ค main หน่อย

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <windows.h>
-
 #include "Header/menu.hpp"
 #include "Header/box.hpp"
 
 using namespace std;
 //someone please explain to me how this code work please...
-void gotoxy(int x, int y)
+/*void gotoxy(int x, int y)
 {
     static HANDLE h = NULL;  
     if (!h) h = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD c = { x, y };  
     SetConsoleCursorPosition(h,c);
-}
+}*/
 
 void textcolor(int color) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color); }
 
 int main()
 {
+    system("color 2f");
+    system("color 0e");
+    char a= 177,b=219;
+    cout<<"\n\n\n\t\t\t\t\tLOADING....";
+    cout<<"\n\n\n";
+    cout<<"\t\t\t\t\t ";
+    for(int i =0;i<=25;i++)
+     cout<<a;
+     Sleep(150);
+    cout<<"\r";
+    cout<<"\t\t\t\t\t ";
+    for(int i=0;i<=25;i++)
+    {
+        cout<<b;
+        Sleep(200);
+    }
+    system("pause>0");
     //**JUST IDEA**
     bool check;
     try
