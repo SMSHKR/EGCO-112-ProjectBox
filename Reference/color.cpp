194dec24a4f179993cee2ac6828c0d775e617f77
SMSHKR <t.benzpun@gmail.com>
2018-05-06 19:48:52 +0700

    Fix color.cpp

--- Reference/color.cpp
+++ Reference/color.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 void textcolor(int color) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color); }
 
-<<<<<<< HEAD
-int main() 
-{
-  
-=======
 int main() {
->>>>>>> 48bee299b5c7c847ef21767cbf9f5de026022ed9
     for(int i=0; i<=256; i++){
         textcolor(i);
         cout << "Color #" << i << endl;
