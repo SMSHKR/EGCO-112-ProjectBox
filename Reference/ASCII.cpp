f7b3d65ba58c4cfe0d5f0f4255456350818f0c56
SMSHKR <t.benzpun@gmail.com>
2018-05-07 11:53:41 +0700

    ASCII Task Added

--- Reference/ASCII.cpp
+++ Reference/ASCII.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
-main()
+int main()
 {
     for (int x = 0; x < 256; x++)
         printf("%c = %d %x\n", x, x, x);
         //c = chracter , d = digit , x = digit16
     scanf(" ");
+    return 0;
 }
