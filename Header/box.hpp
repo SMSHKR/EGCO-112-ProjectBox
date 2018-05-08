29b41c4264e3fcf970f80dd417618fa448adcce2
taninjackie <taninjackie@hotmail.com>
2018-05-08 17:33:14 +0700

    fix logic error (stage.cpp)

--- Header/box.hpp
+++ Header/box.hpp
@@ -8,7 +8,7 @@ private:
 public:
     static int objectCount;
     
-    Box *next;
+    Box *next = '\0';
     Box(bool=false); //constructor
     
     void swap();
--- stage.cpp
+++ stage.cpp
@@ -7,29 +7,35 @@ bool stage(int num, Box *&HEAD) {
     srand(time(NULL));
 
     //Create Linked List of Box
-    bool key = false;
-    int created = 0;
+    bool key = false; //Checked If key generated
+    int created = 0; //Count Box Generate
     while (created < num) {
+        created += 1;
         if (!HEAD) {
             bool check = 0;
-            if (!key) check = rand() % 2; //0, 1
-            if (check != 1) {
+            if (!key) {
+                if (created == num) check = 1; //Case of NONE of Boxes have key
+                else check = rand() % 2; //0, 1
+            }
+            if (check) {
                 HEAD = new Box(true);
                 key = true;
-            } 
+            }
             else HEAD = new Box;
         }
         else {
             bool check = 0;
             Box *walker = HEAD;
             while (walker->next) walker = walker->next;
-            if (!key) check = rand() % 2; //0, 1
-            if (check != 1) {
+            if (!key) {
+                if (created == num) check = 1; //Case of NONE of Boxes have key
+                else check = rand() % 2; //0, 1
+            }
+            if (check) {
                 walker->append(new Box(true));
                 key = true;
             } 
             else walker->append(new Box); 
         }
-        created += 1;
     }
 }
