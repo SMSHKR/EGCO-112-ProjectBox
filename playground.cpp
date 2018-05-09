7cb3c376e4cc2ceab912944e4b1aa63e14025ebe
SMSHKR <t.benzpun@gmail.com>
2018-05-09 07:34:15 +0700

    Testing ASCII Impossible Method at Playground

--- playground.cpp
+++ playground.cpp
@@ -12,7 +12,12 @@ using namespace std;
 int main() {
     
     cout << "For Testing" << endl;
-    cin.get();
+
+    char a = 201; //╔
+    char b = 188; //╝
+    char c = 219; //█
+    cout << a << "\n" << b << "\n" << c << endl;
    
+    cin.get();
     return 0;
 }
