c82cc4aac202f61dd668dbf728c6fa2871e263bc
SMSHKR <t.benzpun@gmail.com>
2018-05-23 17:35:08 +0700

    Fix missing c,d

--- box.cpp
+++ box.cpp
@@ -26,29 +26,36 @@ void Box::draw(short a, short b) {
     x = a;
     y = b;
     gotoxy(x,y);
-<<<<<<< HEAD
+    char c = 219;
+    char d = 205;
     textcolor(15);
     cout <<c<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<c<<endl;
     cout <<c<<d<<d<<d<<d<<d<<c<<c<<c<<c<<d<<d<<d<<d<<d<<c<<endl;
     cout <<c<<"              "<<c<<endl;
     cout <<c<<"              "<<c<<endl;
     cout <<c<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<c<<endl;
-=======
-    cout << "Box" << endl;
->>>>>>> 134d90a1696ef4efc9d3da937d75383a5879a602
 }
 
 void Key::draw(short a, short b) {
     x = a;
     y = b;
     gotoxy(x,y);
-    cout << "Key" << endl;
+    char c = 219;
+    char d = 205;
+    textcolor(14);
+    cout <<c<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<c<<endl;
+    cout <<c<<d<<d<<d<<d<<d<<c<<c<<c<<c<<d<<d<<d<<d<<d<<c<<endl;
+    cout <<c<<"              "<<c<<endl;
+    cout <<c<<"              "<<c<<endl;
+    cout <<c<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<c<<endl;
 }
 
 void Ney::draw(short a, short b) {
     x = a;
     y = b;
     gotoxy(x,y);
+    char c = 219;
+    char d = 205;
     textcolor(12);
     cout <<c<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<d<<c<<endl;
     cout <<c<<d<<d<<d<<d<<d<<c<<c<<c<<c<<d<<d<<d<<d<<d<<c<<endl;
