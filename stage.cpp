f08f057cbd9cd55d53c48ebc9cf190d0e20f6857
taninjackie <taninjackie@hotmail.com>
2018-05-23 10:26:36 +0700

    stage

--- stage.cpp
+++ stage.cpp
@@ -8,6 +8,12 @@
 #include "Header/legacy.hpp"
 using namespace std;
 
+#define KEY_ENTER 13
+#define KEY_UP 72
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+#define KEY_DOWN 80
+
 bool stage(int num) {
 
     Box *HEAD = nullptr;
