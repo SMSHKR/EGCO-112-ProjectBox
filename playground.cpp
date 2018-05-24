1f6da89018c9023f6259383b68b3780893842bbd
taninjackie <taninjackie@hotmail.com>
2018-05-24 15:50:33 +0700

    playground

--- playground.cpp
+++ playground.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 int main() {
-    gotoxy(6,5);
+    /*gotoxy(6,5);
     textcolor(14);
     cout<<" 888888ba   .88888.  dP    dP    dP     dP 888888ba  dP     dP 888888ba   .88888.  dP   dP   dP 888888ba   \n";
     gotoxy(6,6);
@@ -23,8 +23,41 @@ int main() {
     cout<<" 88    .88 Y8.   .8P 88    88    Y8.   .8P 88     88 88     88 88     88 Y8.   .8P 88.d8P8.d8P  88     88  \n";
     gotoxy(6,10);
     cout<<" 88888888P  `8888P'  dP    dP    `Y88888P' dP     dP dP     dP dP     dP  `8888P'  8888' Y88'   dP     dP  \n";
-   
-
+   */
+   textcolor(14);
+    gotoxy(6,5);
+    cout<<"           (        )     )       (                )  (               )          )   (     (        )     )       (     ";
+    gotoxy(6,6);
+    cout<<" (  (      )\ )  ( /(  ( /(       )\ )     (    ( /(  )\ )   (     ( /(       ( /(   )\ )  )\ )  ( /(  ( /(       )\ )  ";
+    gotoxy(6,7);
+    cout<<" )\))(   '(()/(  )\()) )\()) (   (()/(     )\   )\())(()/(   )\    )\()) (    )\()) (()/( (()/(  )\()) )\()) (   (()/(  ";
+    gotoxy(6,8);
+    cout<<"((_)()\ )  /(_))((_)\ ((_)\  )\   /(_))  (((_) ((_)\  /(_))(((_) |((_)\  )\  ((_)\   /(_)) /(_))((_)\ ((_)\  )\   /(_))";
+    gotoxy(6,9);
+    cout<<"_(())\_)()(_))   _((_) _((_)((_) (_))    )\___  _((_)(_))  )\___ |_ ((_)((_)  _((_) (_))_ (_))   _((_) _((_)((_) (_)) ";
+    gotoxy(6,10);
+    cout<<"W     W III N   N N   N EEEE RRRR       CCC H  H III  CCC K  K EEEE N   N     DDD  III N   N N   N EEEE RRRR ";
+    gotoxy(6,11);
+    cout<<"W     W  I  NN  N NN  N E    R   R     C    H  H  I  C    K K  E    NN  N     D  D  I  NN  N NN  N E    R   R";
+    gotoxy(6,12);
+    cout<<"W  W  W  I  N N N N N N EEE  RRRR      C    HHHH  I  C    KK   EEE  N N N     D  D  I  N N N N N N EEE  RRRR ";
+    gotoxy(6,13);
+    cout<<" W W W   I  N  NN N  NN E    R R       C    H  H  I  C    K K  E    N  NN     D  D  I  N  NN N  NN E    R R   ";
+    gotoxy(6,14);
+    cout<<"  W W   III N   N N   N EEEE R  RR      CCC H  H III  CCC K  K EEEE N   N     DDD  III N   N N   N EEEE R  RR ";
+    /*
+    textcolor(14);
+    gotoxy(55,10);
+    cout<<"N   N  OOO   OOO  BBBB " ;
+    gotoxy(55,11);
+    cout<<"NN  N O   O O   O B   B" ;
+    gotoxy(55,12);
+    cout<<"N N N O   O O   O BBBB " ;
+    gotoxy(55,13);
+    cout<<"N  NN O   O O   O B   B" ;
+    gotoxy(55,14);
+    cout<<"N   N  OOO   OOO  BBBB " ;
+*/
 
     cin.get();
     return 0;
